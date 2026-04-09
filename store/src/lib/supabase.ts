d7f5c2b04aa5cba6135194d2568089edc1c343b5
Amit Kumar <amitcreation48@gmail.com>
2026-04-10 00:01:49 +0530

    Initial commit

--- store/src/lib/supabase.ts
+++ store/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vybnneqxipninllzsfed.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5Ym5uZXF4aXBuaW5sbHpzZmVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDkxMzMsImV4cCI6MjA5MTMyNTEzM30.yzltlSWVJOuhjDSQFW44TfH4pKqcyi0ZbW94aHu8oTE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
